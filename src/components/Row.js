8ea7590a0f1807c43a9f70ff5bf70a1f3de13a4d
Sagar Ghimire <gmrsagar@gmail.com>
2020-09-19 11:36:07 +1000

    Add css for row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -25,6 +25,7 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${baseUrl}${movie.poster_path}`}
             alt={movie.name}
