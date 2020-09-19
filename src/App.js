59c55f74ec8692d50d5b8bbe8d93a934cf0981f1
Sagar Ghimire <gmrsagar@gmail.com>
2020-09-19 15:02:58 +1000

    Add original block

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
-import Row from './components/Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi There</h1>
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUr={requests.fetchTrending}l/>
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUr={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Romance" fetchUrl-={requests.fetchRomanceMovies} />
+      <Row title="Horror" fetchUrl-={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl-={requests.fetchDocumentaries} />
+      <Row title="Action" fetchUrl-={requests.fetchActionMovies} />
     </div>
   );
 }
